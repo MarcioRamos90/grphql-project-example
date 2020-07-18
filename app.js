148a9737fab6050f3ba1efd461f28ca96ce35afd
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-07-18 13:00:10 -0300

    Config graphql endpoint

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
 const app = express();
+
+const schema = require("./schema/schema");
+
+app.use(
+  "/graphql",
+  graphqlHTTP.graphqlHTTP({
+    graphiql: true,
+    schema,
+  })
+);
+
 app.listen(4000, () => {
   console.log("Listening for requests on port: ", 4000);
 });
